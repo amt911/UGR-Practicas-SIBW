b0a81adf38434cab4a97f34500162616f82f9617
Andrés Merlo Trujillo <a.merlo.truji10@gmail.com>
2022-03-26 11:11:19 +0100

    Medio funciona el poner los comentarios

--- P2/static/js/producto.js
+++ P2/static/js/producto.js
@@ -1,6 +1,6 @@
 
 
-var desplegableComentarios=document.getElementById("comentarios");
+var desplegableComentarios=document.getElementById("comentarios-submit-container");
 
 var botonComentarios=document.getElementById("boton-comentario");
 
@@ -19,3 +19,37 @@ function expandirComentarios(){
 
 botonComentarios.addEventListener("click", expandirComentarios);
 
+
+
+//Ahora viene la parte de meter el comentario :(
+
+//var comentarios=document.getElementById("comentarios")
+
+function subirComentario(){
+    var nombre=document.getElementById("nombre")
+    var email=document.getElementById("email")
+    //var comentario=document.getElementById("comentario-nuevo")
+    var comentarios=document.getElementsByClassName("comentario-container")
+    var fecha=new Date();
+
+    alert(comentarios.length)
+
+    //ARREGLAR ESTO QUE CREO QUE ESTA MAL
+    comentarios[0].insertAdjacentHTML("afterbegin", 
+    "<div class=\"comentario-container\">"+
+        "<div class=\"nombre-comentario\">"+
+            "<h4>"+nombre.value+"</h4>"+
+        "</div>"+
+        "<div class=\"fecha\">"+
+            fecha.getDay()+"-"+fecha.getMonth()+"-"+fecha.getFullYear()+" "+fecha.getHours()+":"+fecha.getMinutes()+
+        "</div>"+
+        "<div class=\"contenido-comentario\">"+
+            nombre.value+
+        "</div>"+
+    "</div>")
+}
+
+
+var botonSubmit=document.getElementById("submit")
+
+botonSubmit.addEventListener("click", subirComentario);
